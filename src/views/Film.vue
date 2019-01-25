73063a9d06db6a100b6285762c2e2b528bbadfd4
Daniel <dynnej99@gmail.com>
2019-01-25 15:14:37 +0100

    Åldersgräns, information om filmer, txt color, json koppling med Michal.

--- src/views/Film.vue
+++ src/views/Film.vue
@@ -1,126 +1,171 @@
 <template>
-    <div class="info">
-        <div class="title">
-            <h2 class="display-4 infoTitel">{{movie.title}}</h2>
-            <hr class="break">
-            <div class="videoPadding">
-                <div class="videoWrapper">
-                    <iframe width=560 height="349" :src="'https://www.youtube.com/embed/' + movie.youtubeTrailers" frameborder="5" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-                    </iframe>
-                </div>
-            </div>
-            <hr class="break">
-            <p class="description">{{movie.description}}</p>
+  <div class="info">
+    <div class="title">
+      <h2 class="display-4 infoTitel">{{movie.title}}</h2>
+      <hr class="break">
+      <div class="videoPadding">
+        <div class="videoWrapper">
+          <iframe
+            width="560"
+            height="349"
+            :src="'https://www.youtube.com/embed/' + movie.youtubeTrailers"
+            frameborder="5"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
         </div>
-        <ul class="infoList">
-            <li><b>Produktion År: </b>{{movie.productionYear}}</li>
-            <li><b>Längd: </b>{{movie.length}}</li>
-            <li><b>Genre: </b>{{movie.genre}}</li>
-            <li><b>Språk:</b> {{movie.language}}</li>
-            <li><b>Undertext:</b> {{movie.subtitles}}</li>
-            <li><b>Producent:</b> {{movie.director}}</li>
-            <li><b>Skådespelare:</b> {{movie.actors}}</li>
+      </div>
 
-        </ul>
-        <button type="button" class="bookingBtn btn btn-outline-success">Boka Nu</button>
+      <div class="filmInformation">{{movie.length}} | {{movie.genre}} | {{movie.ageRestricton}} år</div>
+
+      <hr class="break">
+      <p class="description">{{movie.description}}</p>
     </div>
+    <ul class="infoList">
+      <li>
+        <b>Produktion År:</b>
+        {{movie.productionYear}}
+      </li>
+      <li>
+        <b>Längd:</b>
+        {{movie.length}}
+      </li>
+      <li>
+        <b>Genre:</b>
+        {{movie.genre}}
+      </li>
+      <li>
+        <b>Språk:</b>
+        {{movie.language}}
+      </li>
+      <li>
+        <b>Undertext:</b>
+        {{movie.subtitles}}
+      </li>
+      <li>
+        <b>Producent:</b>
+        {{movie.director}}
+      </li>
+      <li>
+        <b>Skådespelare:</b>
+        {{movie.actors}}
+      </li>
+    </ul>
+    <button type="button" class="bookingBtn btn btn-outline-success">Boka Nu</button>
+  </div>
 </template>
 
 <script>
-    import movies from '../assets/json/movies.json'
-    export default {
-        data() {
-            return {
-                movie: movies[this.$route.params.index]
-            }
-        }
-    }
+import movies from "../assets/json/movies.json";
+export default {
+  data() {
+    return {
+      movie: movies[this.$route.params.index]
+    };
+  }
+};
 </script>
+
 <style>
-    .info {
-        padding-top: 15px;
-        color: white;
-        margin: 15px;
-        margin-top: 20px;
-        background-color: rgba(129, 131, 132, .3);
-    }
+.info {
+  padding-top: 15px;
+  color: white;
+  margin: 15px;
+  margin-top: 20px;
+  background-color: rgba(129, 131, 132, 0.3);
+}
 
-    .videoPadding {
-        padding: 0 20%;
-    }
+ul {
+  max-width: 700px;
+}
 
-    .videoWrapper {
-        position: relative;
-        padding-bottom: 56.25%;
-        /* 16:9 */
-        padding-top: 25px;
-        height: 0;
-        box-shadow: 0px 0px 8px 2px rgb(26, 29, 33);
-    }
+ul li {
+  color: rgb(216, 216, 216);
+}
 
-    .videoWrapper iframe {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-    }
+ul li b {
+  color: white;
+}
 
-    .break {
-        border: solid 0.5px;
-        color: white;
-        margin: 15px 30px;
-    }
+.videoPadding {
+  padding: 0 20%;
+}
 
-    .description {
-        margin: 15px 30px;
-        text-align: left;
-    }
+.videoWrapper {
+  position: relative;
+  padding-bottom: 56.25%;
+  /* 16:9 */
+  padding-top: 25px;
+  height: 0;
+  box-shadow: 0px 0px 8px 2px rgb(26, 29, 33);
+}
 
-    .infoList {
-        list-style-type: none;
-        text-align: left;
-        margin-top: 15px;
-    }
+.videoWrapper iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
 
-    .bookingBtn {
-        padding: 5px 50px;
-        margin-bottom: 15px;
-    }
+.break {
+  border: solid 0.5px;
+  color: white;
+  margin: 15px 30px;
+}
 
+.description {
+  margin: 15px 30px;
+  text-align: left;
+}
 
-    @media screen and (max-width: 900px) {
-        .videoPadding {
-            padding: 0 10%;
-        }
+.infoList {
+  list-style-type: none;
+  text-align: left;
+  margin-top: 15px;
+}
 
-        .infoTitel {
-            font-size: 200%;
-        }
-    }
+.bookingBtn {
+  padding: 5px 50px;
+  margin-bottom: 15px;
+}
 
-    @media screen and (max-width: 500px) {
-        .info {
-            margin: 5px;
-        }
+.filmInformation {
+  margin-top: 2.5vh;
+  font-size: 24px;
+}
 
-        .videoPadding {
-            padding: 0 5%;
-        }
+@media screen and (max-width: 900px) {
+  .videoPadding {
+    padding: 0 10%;
+  }
 
-        .infoTitel {
-            font-size: 180%;
-            margin-top: 15px;
-        }
+  .infoTitel {
+    font-size: 200%;
+  }
+}
 
-        .description {
-            font-size: 85%;
-            margin: 15px;
-        }
+@media screen and (max-width: 500px) {
+  .info {
+    margin: 5px;
+  }
 
-        .break {
-            margin: 15px;
-        }
-    }
+  .videoPadding {
+    padding: 0 5%;
+  }
 
+  .infoTitel {
+    font-size: 180%;
+    margin-top: 15px;
+  }
+
+  .description {
+    font-size: 85%;
+    margin: 15px;
+  }
+
+  .break {
+    margin: 15px;
+  }
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,256 +1,271 @@
 <template>
-    <div>
-        <div class="commingSoon">
-            <img class="commingSoonImg" src="https://images.alphacoders.com/954/954739.jpg" alt="Comming Soon" width="100%">
-        </div> -->
-        <div class="searchContainer">
-            <form class="form-inline">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <div class="btn-group">
-                    <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <button class="dropdown-item" type="button">Action</button>
-                    </div>
-                </div>
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </div>
-        <!--Time container-->
-        <div class="movieTimeContainer">
-            <div class="movieTime">
-                <h5>12:30</h5>
-                <h5>9/11</h5>
-            </div>
-            <hr>
-            <!--Movie grid-->
-            <div class="movieGridContainer">
-                <!--Movie-->
-                <div v-for="(item, i) in items" :key="i">
-                    <div class="movieContainer" :style="{ backgroundImage: `url('${item.images}')` }">
-                        <h2 class="movieTitel">{{item.title}}</h2>
-                        <!--More button-->
-                        <div class="corner">
-                            <router-link :to="'/film/' + i" class="nav-link"><span>Mer</span></router-link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="movieTimeContainer">
-            <div class="movieTime">
-                <h5>20:45</h5>
-                <h5>9/12</h5>
-            </div>
-            <hr>
-            <!--Movie grid-->
-            <div class="movieGridContainer">
-                <!--Movie-->
-                <div v-for="(item, i) in items" :key="i">
-                    <div class="movieContainer" :style="{ backgroundImage: `url('${item.images}')` }">
-                        <h2 class="movieTitel">{{item.title}}</h2>
-                        <!--More button-->
-                        <div class="corner">
-                            <router-link :to="'/film/' + i" class="nav-link"><span>Mer</span></router-link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="komandeFilmGridContainer">
-            <h1>Kommande film</h1>
-            <hr>
-            <div class="row">
-                <div v-for="(kommandeFilmer, i) in kommandeFilmer" :key="i" class="kommandeFilmContainer" :style="{ backgroundImage: `url('${kommandeFilmer.image}')` }">
-                    <h2>{{kommandeFilmer.title}}</h2>
-
-                </div>
-            </div>
-        </div>
+  <div>
+    <div class="commingSoon">
+      <img
+        class="commingSoonImg"
+        src="https://images.alphacoders.com/954/954739.jpg"
+        alt="Comming Soon"
+        width="100%"
+      >
     </div>
+    <div class="searchContainer">
+      <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <div class="btn-group">
+          <button
+            type="button"
+            class="btn btn-secondary dropdown-toggle"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          ></button>
+          <div class="dropdown-menu dropdown-menu-right">
+            <button class="dropdown-item" type="button">Action</button>
+          </div>
+        </div>
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+    <!--Time container-->
+    <div class="movieTimeContainer">
+      <div class="movieTime">
+        <h5>12:30</h5>
+        <h5>9/11</h5>
+      </div>
+      <hr>
+      <!--Movie grid-->
+      <div class="movieGridContainer">
+        <!--Movie-->
+        <div v-for="(item, i) in items" :key="i">
+          <div class="movieContainer" :style="{ backgroundImage: `url('${item.images}')` }">
+            <h2 class="movieTitel">{{item.title}}</h2>
+            <!--More button-->
+            <div class="corner">
+              <router-link :to="'/film/' + i" class="nav-link">
+                <span>Mer</span>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="movieTimeContainer">
+      <div class="movieTime">
+        <h5>20:45</h5>
+        <h5>9/12</h5>
+      </div>
+      <hr>
+      <!--Movie grid-->
+      <div class="movieGridContainer">
+        <!--Movie-->
+        <div v-for="(item, i) in items" :key="i">
+          <div class="movieContainer" :style="{ backgroundImage: `url('${item.images}')` }">
+            <h2 class="movieTitel">{{item.title}}</h2>
+            <!--More button-->
+            <div class="corner">
+              <router-link :to="'/film/' + i" class="nav-link">
+                <span>Mer</span>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="komandeFilmGridContainer">
+      <h1>Kommande film</h1>
+      <hr>
+      <div class="row">
+        <div
+          v-for="(kommandeFilmer, i) in kommandeFilmer"
+          :key="i"
+          class="kommandeFilmContainer"
+          :style="{ backgroundImage: `url('${kommandeFilmer.image}')` }"
+        >
+          <h2>{{kommandeFilmer.title}}</h2>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    import movies from '../assets/json/movies.json'
-    import kommandeFilmer from '../assets/json/kommandeFilmer.json'
+import movies from "../assets/json/movies.json";
+import kommandeFilmer from "../assets/json/kommandeFilmer.json";
 
-    export default {
-        name: 'hello',
-        data() {
-            return {
-                items: movies,
-                kommandeFilmer: kommandeFilmer
-            }
-        }
-    }
-
+export default {
+  name: "hello",
+  data() {
+    return {
+      items: movies,
+      kommandeFilmer: kommandeFilmer
+    };
+  }
+};
 </script>
 
 <style>
-    /*    Comming Soon     */
-    .commingSoonImg {
-        mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 1), rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
+/*    Comming Soon     */
+.commingSoonImg {
+  mask-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 1),
+    rgba(0, 0, 0, 1),
+    rgba(0, 0, 0, 1),
+    rgba(0, 0, 0, 0)
+  );
+}
 
-    }
+/*    Searchbar     */
+.searchContainer {
+  margin: 15px;
+  background-color: rgba(129, 131, 132, 0.3);
+}
 
-    /*    Searchbar     */
-    .searchContainer {
-        margin: 15px;
-        background-color: rgba(129, 131, 132, .3);
-    }
+.searchContainer .form-inline {
+  padding: 15px;
+  display: flex;
+}
 
-    .searchContainer .form-inline {
-        padding: 15px;
-        display: flex;
-    }
+.searchContainer .form-inline input {
+  padding: 15px;
+  max-width: 400px;
+  flex: 1;
+}
 
-    .searchContainer .form-inline input {
-        padding: 15px;
-        max-width: 400px;
-        flex: 1;
-    }
+.btn-group button {
+  margin-right: 15px;
+}
 
-    .btn-group button {
-        margin-right: 15px;
-    }
+/*    Movie containers     */
+.movieTimeContainer {
+  margin: 15px;
+  background-color: rgba(129, 131, 132, 0.3);
+}
 
-    /*    Movie containers     */
-    .movieTimeContainer {
-        margin: 15px;
-        background-color: rgba(129, 131, 132, .3);
-    }
+.movieTimeContainer h5 {
+  text-align: left;
+  color: white;
+  padding: 30px 30px 0px;
+}
 
-    .movieTimeContainer h5 {
-        text-align: left;
-        color: white;
-        padding: 30px 30px 0px;
-    }
+.movieTimeContainer hr {
+  background-color: white;
+  margin: 0 30px;
+}
 
-    .movieTimeContainer hr {
-        background-color: white;
-        margin: 0 30px;
-    }
+.movieGridContainer {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
 
-    .movieGridContainer {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-    }
-    
-    .movieTimeContainer {
-        margin: 15px;
-        background-color: rgba(129, 131, 132, .3);
-    }
+.movieTimeContainer {
+  margin: 15px;
+  background-color: rgba(129, 131, 132, 0.3);
+}
 
-    .row {
-        margin-left: 4px !important;
-        margin-right: 4px !important;
+.row {
+  margin-left: 4px !important;
+  margin-right: 4px !important;
+}
 
-    }
+.komandeFilmGridContainer {
+  margin: 15px;
+  background-color: rgba(129, 131, 132, 0.3);
+}
 
-    .komandeFilmGridContainer{
-        margin: 15px;
-        background-color: rgba(129, 131, 132, .3);
-    }
-    
-    .kommandeFilmContainer h2 {
-        background: rgba(26, 29, 33, .9);
-        border-width: 40px;
-        color: white;
-        font-size: 150%;
-        padding: 5px;
-    }
-    
-    .komandeFilmGridContainer hr {
-        background-color: white;
-        margin: 0 30px;
-    }
+.kommandeFilmContainer h2 {
+  background: rgba(26, 29, 33, 0.9);
+  border-width: 40px;
+  color: white;
+  font-size: 150%;
+  padding: 5px;
+}
 
-    h1 {
-        color: white !important;
-        padding-top: 15px;
-    }
+.komandeFilmGridContainer hr {
+  background-color: white;
+  margin: 0 30px;
+}
 
-    .kommandeFilmContainer {
-        margin: 15px;
-        height: 300px;
-        width: 280px;
-        margin: 15px auto;
-        background-size: cover;
-        box-shadow: 0px 0px 8px 2px rgb(26, 29, 33);
-    }
+h1 {
+  color: white !important;
+  padding-top: 15px;
+}
 
-    .movieTime {
-        display: flex;
-    }
+.kommandeFilmContainer {
+  margin: 15px;
+  height: 300px;
+  width: 280px;
+  margin: 15px auto;
+  background-size: cover;
+  box-shadow: 0px 0px 8px 2px rgb(26, 29, 33);
+}
 
-    .movieTime h5 {
-        flex: 1;
-    }
+.movieTime {
+  display: flex;
+}
 
-    .movieTime h5:nth-child(2) {
-        text-align: right;
-    }
+.movieTime h5 {
+  flex: 1;
+}
 
-    .movieContainer {
-        margin: 15px auto;
-        background-size: cover;
-        background-position: center;
-        position: relative;
-        background-color: grey;
-        width: calc(100% - 60px);
-        max-width: 400px;
-        min-height: 600px;
-        box-shadow: 0px 0px 8px 2px rgb(26, 29, 33);
-    }
+.movieTime h5:nth-child(2) {
+  text-align: right;
+}
 
-    .movieTitel {
-        background-color: rgba(26, 29, 33, .9);
-        color: white;
-        font-size: 150%;
-        padding: 5px;
-    }
+.movieContainer {
+  margin: 15px auto;
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  background-color: grey;
+  width: calc(100% - 60px);
+  max-width: 400px;
+  min-height: 600px;
+  box-shadow: 0px 0px 8px 2px rgb(26, 29, 33);
+}
 
-    .corner {
-        width: 0;
-        height: 0;
-        border-bottom: 150px solid rgba(26, 29, 33, .9);
-        border-top: 150px solid transparent;
-        border-left: 150px solid transparent;
-        position: absolute;
-        right: 0;
-        bottom: 0;
-    }
+.movieTitel {
+  background-color: rgba(26, 29, 33, 0.9);
+  color: white;
+  font-size: 150%;
+  padding: 5px;
+}
 
-    .corner span {
-        cursor: pointer;
-        position: absolute;
-        bottom: -110px;
-        width: 100px;
-        left: -100px;
-        text-align: center;
-        font-size: 16px;
-        display: block;
-        color: white;
-    }
+.corner {
+  width: 0;
+  height: 0;
+  border-bottom: 150px solid rgba(26, 29, 33, 0.9);
+  border-top: 150px solid transparent;
+  border-left: 150px solid transparent;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+}
 
-    @media screen and (max-width: 900px) {
-        .movieGridContainer {
-            display: grid;
-            grid-template-columns: 1fr;
-        }
-    }
+.corner span {
+  cursor: pointer;
+  position: absolute;
+  bottom: -110px;
+  width: 100px;
+  left: -100px;
+  text-align: center;
+  font-size: 16px;
+  display: block;
+  color: white;
+}
 
-    @media screen and (min-width: 1400px) {
-        .movieGridContainer {
-            display: grid;
-            grid-template-columns: 1fr 1fr 1fr;
-        }
-    }
-    
+@media screen and (max-width: 900px) {
+  .movieGridContainer {
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+}
 
-  
-
-  
-
-
+@media screen and (min-width: 1400px) {
+  .movieGridContainer {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+}
 </style>
